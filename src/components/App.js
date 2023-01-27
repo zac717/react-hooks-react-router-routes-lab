496bde65c34b60de4ccb05bce6c8ea83928a55ab
zac717 <isaacongera717@gmail.com>
2023-01-27 09:40:24 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
@@ -6,8 +7,26 @@ import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
-function App() {
-  return <div>{/*{code here}*/}</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/directors">
+          <Directors />
+        </Route>
+        <Route path="/actors">
+          <Actors />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
